b300024aa87103720cef67102e17b62699552bf7
Zejie Yao <ZYao3@slb.com>
2022-10-26 14:48:31 +0800

    test: do not use test double

--- CashRegister/SpyPrinter.cs
+++ CashRegister/SpyPrinter.cs
@@ -1,13 +1,10 @@
 namespace CashRegister
 {
-	public class Printer
+	public class SpyPrinter : Printer
 	{
-		public bool HasPrinted { get; set; }
-
 		public void Print(string content)
 		{
 			// send message to a real printer
-			HasPrinted = true;
 		}
 	}
 }
